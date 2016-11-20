78df6f7dd4b599e95b53875700ba954b0d756f1b
CodeDead <admin@codedead.com>
2016-11-21 00:12:37 +0100

    * Added load settings method to settings window

--- Advanced PortChecker/Windows/SettingsWindow.xaml.cs
+++ Advanced PortChecker/Windows/SettingsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Advanced_PortChecker.Classes;
+﻿using System;
+using System.Windows;
+using Advanced_PortChecker.Classes;
 
 namespace Advanced_PortChecker.Windows
 {
@@ -9,9 +11,7 @@ namespace Advanced_PortChecker.Windows
     {
 
         #region Variables
-
         private MainWindow _mw;
-
         #endregion
 
         public SettingsWindow(MainWindow mainWindow)
@@ -20,11 +20,34 @@ namespace Advanced_PortChecker.Windows
 
             InitializeComponent();
             ChangeVisualStyle();
+            LoadSettings();
         }
 
+        /// <summary>
+        /// Change the visual style of the controls, depending on the settings.
+        /// </summary>
         private void ChangeVisualStyle()
         {
             StyleManager.ChangeStyle(this);
+        }
+
+        /// <summary>
+        /// Change the GUI to represent the current settings.
+        /// </summary>
+        private void LoadSettings()
+        {
+            try
+            {
+                ChbAutoUpdate.IsChecked = Properties.Settings.Default.AutoUpdate;
+
+                ChbStyle.SelectedValue = Properties.Settings.Default.VisualStyle;
+                CpMetroBrush.Color = Properties.Settings.Default.MetroColor;
+                IntBorderThickness.Value = Properties.Settings.Default.BorderThickness;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Advanced PortChecker", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
