beae2cee147933c913761f76ca92bdf33eea4f7d
CodeDead <admin@codedead.com>
2024-10-08 01:10:48 +0200

    feat: added subnet scanning, dependency upgrades, refactoring

---
+++ src/components/PortInput/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+const PortInput = ({
+  label, port, disabled, onKeyDown, onChange,
+}) => (
+  <TextField
+    id="start-port-basic"
+    label={label}
+    variant="outlined"
+    value={port}
+    disabled={disabled}
+    type="number"
+    min={0}
+    max={65535}
+    pattern="[0-9]"
+    fullWidth
+    onChange={onChange}
+    onKeyDown={onKeyDown}
+  />
+);
+
+export default PortInput;
